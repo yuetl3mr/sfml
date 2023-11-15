7d8a8c30b62a819c698da28d9d6379c65ead2144
yuetl3mr <kimthach.work@gmail.com>
2023-11-16 00:45:13 +0700

    fk bug dropdown menu

--- dropdownmenu.cpp
+++ dropdownmenu.cpp
@@ -2,10 +2,17 @@
 #include <iostream>
 #include <vector>
 
+const float MENU_SPEED = 5.0f;
+
 int main() {
     sf::RenderWindow window(sf::VideoMode(200, 200), "Dropdown Menu");
 
     bool isMenuOpen = false;
+    bool isMenuMoving = false;
+    float menuHeight = 0.0f;
+    float targetHeight = 0.0f;
+    sf::Clock clock;
+
     std::vector<sf::Text> menuItems;
 
     sf::Font font;
@@ -32,6 +39,8 @@ int main() {
     }
 
     while (window.isOpen()) {
+        sf::Time deltaTime = clock.restart();
+
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
@@ -43,14 +52,22 @@ int main() {
                     // Toggle menu open/close
                     if (selectedItem.getGlobalBounds().contains(mousePos)) {
                         isMenuOpen = !isMenuOpen;
+                        isMenuMoving = true;
+                        if (isMenuOpen) {
+                            targetHeight = static_cast<float>(menuItems.size() * 30);
+                        } else {
+                            targetHeight = 0.0f;
+                        }
                     }
 
                     // Handle item selection
-                    if (isMenuOpen) {
+                    if (isMenuOpen && !isMenuMoving) {
                         for (size_t i = 0; i < menuItems.size(); ++i) {
                             if (menuItems[i].getGlobalBounds().contains(mousePos)) {
                                 selectedItem.setString(menuItems[i].getString());
                                 isMenuOpen = false;
+                                targetHeight = 0.0f;
+                                isMenuMoving = true;
                                 break;
                             }
                         }
@@ -59,14 +76,26 @@ int main() {
             }
         }
 
+        if (isMenuMoving) {
+            float distance = targetHeight - menuHeight;
+            if (std::abs(distance) > 1.0f) {
+                menuHeight += distance * MENU_SPEED * deltaTime.asSeconds();
+            } else {
+                menuHeight = targetHeight;
+                isMenuMoving = false;
+            }
+        }
+
         window.clear(sf::Color::Black);
 
+        // Draw selected item
+        selectedItem.setPosition(0, 0);
         window.draw(selectedItem);
 
-        if (isMenuOpen) {
-            for (const auto& item : menuItems) {
-                window.draw(item);
-            }
+        // Draw menu items
+        for (size_t i = 0; i < menuItems.size(); ++i) {
+            menuItems[i].setPosition(0, 30 * (i + 1) + menuHeight);
+            window.draw(menuItems[i]);
         }
 
         window.display();
